03c5450fb473f652ccb9a43e27916eebc7dabe74
sheinasim <ssim8@hawaii.edu>
2021-09-01 11:03:12 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# YACaSSF: Yet Another Contig and Scaffold Stats Finder 
+Report number of assembly pieces (contigs or scaffolds) and length of piece to make up a genome percentage of your choosing.
+
+This script is designed to be run on a genome assembly in .fasta format 
+
+Dependencies:
+
+* Python3 
+* Biopython
+* Python pandas package
+
+> export PATH=$PATH:[PATH TO YACaSSF]  
+
+### Usage
+  
+> python nlStats.py \<genome percentage as an integer (50 for N50, 95 for N95)\> \<assembly.fasta\>
+
+If no arguments are provided, the script will return help message.
+
+## Outputs
+
+* Number of pieces and length of piece required for a percentage of your assembly size of your choosing as stdout 
+
+### Citation
+
+If this script is useful to you, please cite the following in your publication:
+
+```
+@software{YACaSSF,
+  author = {Sim, Sheina B.},
+  title = {YACaSSF},
+  url = {https://github.com/sheinasim/YACaSSF}
+}
+```
+
+Sheina B. Sim  
+USDA-ARS  
+US Pacific Basin Agricultural Research Service  
+Hilo, Hawaii, 96720 USA  
+sheina.sim@usda.gov  
+
+This script is in the public domain in the United States per 17 U.S.C. § 105
---
+++ nlStats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import argparse
+import pandas as pd
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument("genomePercentage", type=int, help="Percentage of genome you want reported, eg: 95 for N95/L95")
+parser.add_argument("fasta", help=".fasta file you want the stats of.")
+args = parser.parse_args()
+
+def findNL(fasta, n):
+	df = pd.DataFrame(columns=('Sequence', 'Length'))
+	for record in SeqIO.parse(fasta, "fasta"):
+		df = df.append({'Sequence' : record.name, 'Length' : len(record.seq)}, ignore_index = True)
+	df['Length'] = df['Length'].apply(pd.to_numeric)
+	df_sorted = df.sort_values('Length', ascending=False).reset_index()
+	df_sorted = df_sorted[['Sequence', 'Length']]
+	df_sorted['Cumulative Length'] = df_sorted['Length'].cumsum()
+	
+	asm_size = sum(df_sorted['Length'])
+	prop_asm_size = asm_size*(n/100)
+
+	idx = (df_sorted['Cumulative Length'] > prop_asm_size).argmax()
+	ctglength = df_sorted['Length'].loc[idx]
+
+	if ctglength > 1000 & ctglength < 1000000:
+		length = ctglength/1000
+		lengthstr = str(length) + "KB"
+	elif ctglength > 1000000:
+		length = ctglength/1000000
+		lengthstr = str(length) + "MB"
+	fastaname = args.fasta.split('/')[-1]
+	N_outstr = "N" + str(n) + " : " + str(idx+1)
+
+	L_outstr = "L" + str(n) + " : " + lengthstr 
+
+	print(fastaname + "\n" + N_outstr + "\n" + L_outstr)
+
+findNL(args.fasta, args.genomePercentage)
